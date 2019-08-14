88fd173c4b09592da1b4a8a401c9d853cd7b1550
Terry Reynolds <terrancejreynolds@gmail.com>
2019-08-14 19:40:10 -0400

    added 2px to bio para right margin

--- src/components/bio.js
+++ src/components/bio.js
@@ -53,7 +53,7 @@ const Bio = () => {
         style={{
           marginBottom: 0,
           minWidth: 170,
-          paddingRight: "10px",
+          paddingRight: "12px",
         }}
       >
         <a
