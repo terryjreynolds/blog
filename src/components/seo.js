ad5174260180c7d0435883b87eba3659018794dc
Terry Reynolds <terrancejreynolds@gmail.com>
2019-08-18 13:52:42 -0400

    added comment to seo

--- src/components/seo.js
+++ src/components/seo.js
@@ -9,7 +9,7 @@ import React from "react"
 import PropTypes from "prop-types"
 import Helmet from "react-helmet"
 import { useStaticQuery, graphql } from "gatsby"
-
+//seo
 function SEO({ description, lang, meta, title, image }) {
   const { site } = useStaticQuery(
     graphql`
